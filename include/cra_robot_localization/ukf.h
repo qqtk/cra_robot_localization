b7ee0178ea6c6e0f7cf0eea8644e460b8bf68060
qqtk <qilong.cn@163.com>
2016-08-11 11:02:02 +0800

    Create ukf.h

---
+++ include/cra_robot_localization/ukf.h
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) 2014, 2015, 2016, Charles River Analytics, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ */
+
+#ifndef cra_robot_localization_UKF_H
+#define cra_robot_localization_UKF_H
+
+#include "cra_robot_localization/filter_base.h"
+
+#include <fstream>
+#include <vector>
+#include <set>
+#include <queue>
+
+namespace RobotLocalization
+{
+
+//! @brief Unscented Kalman filter class
+//!
+//! Implementation of an unscenter Kalman filter (UKF). This
+//! class derives from FilterBase and overrides the predict()
+//! and correct() methods in keeping with the discrete time
+//! UKF algorithm. The algorithm was derived from the UKF
+//! Wikipedia article at
+//! (http://en.wikipedia.org/wiki/Kalman_filter#Unscented_Kalman_filter)
+//! ...and this paper:
+//! J. J. LaViola, Jr., 鈥淎 comparison of unscented and extended Kalman
+//! filtering for estimating quaternion motion,鈥in Proc. American Control
+//! Conf., Denver, CO, June 4鈥, 2003, pp. 2435鈥440
+//! Obtained here: http://www.cs.ucf.edu/~jjl/pubs/laviola_acc2003.pdf
+//!
+class Ukf: public FilterBase
+{
+  public:
+    //! @brief Constructor for the Ukf class
+    //!
+    //! @param[in] args - Generic argument container. It is assumed
+    //! that args[0] constains the alpha parameter, args[1] contains
+    //! the kappa parameter, and args[2] contains the beta parameter.
+    //!
+    explicit Ukf(std::vector<double> args);
+
+    //! @brief Destructor for the Ukf class
+    //!
+    ~Ukf();
+
+    //! @brief Carries out the correct step in the predict/update cycle.
+    //!
+    //! @param[in] measurement - The measurement to fuse with our estimate
+    //!
+    void correct(const Measurement &measurement);
+
+    //! @brief Carries out the predict step in the predict/update cycle.
+    //!
+    //! Projects the state and error matrices forward using a model of
+    //! the vehicle's motion.
+    //!
+    //! @param[in] referenceTime - The time at which the prediction is being made
+    //! @param[in] delta - The time step over which to predict.
+    //!
+    void predict(const double referenceTime, const double delta);
+
+  protected:
+    //! @brief The UKF sigma points
+    //!
+    //! Used to sample possible next states during prediction.
+    //!
+    std::vector<Eigen::VectorXd> sigmaPoints_;
+
+    //! @brief This matrix is used to generate the sigmaPoints_
+    //!
+    Eigen::MatrixXd weightedCovarSqrt_;
+
+    //! @brief The weights associated with each sigma point when generating
+    //! a new state
+    //!
+    std::vector<double> stateWeights_;
+
+    //! @brief The weights associated with each sigma point when calculating
+    //! a predicted estimateErrorCovariance_
+    //!
+    std::vector<double> covarWeights_;
+
+    //! @brief Used in weight generation for the sigma points
+    //!
+    double lambda_;
+
+    //! @brief Used to determine if we need to re-compute the sigma
+    //! points when carrying out multiple corrections
+    //!
+    bool uncorrected_;
+};
+
+}  // namespace RobotLocalization
+
+#endif  // cra_robot_localization_UKF_H
